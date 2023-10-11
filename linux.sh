69415dc2baf4d07d2afc972059b942f55a806e77
Atharv <natuatharv21@gmail.com>
2023-10-12 01:43:28 +0530

    Updated Build Script

--- linux.sh
+++ linux.sh
@@ -6,7 +6,7 @@ clear
 cd ./bin/
 
 # Uses Rocm 5.7.0
-g++ -w -c ../src/CLFW/*.cpp ../test/*cpp -I "/opt/rocm-5.7.0/include/"
+g++ -w -c ../src/CLFW/*.cpp ../test/*.cpp -I "/opt/rocm-5.7.0/include/"
 
 g++ -o VecAdd *.o -L "/opt/rocm-5.7.0/lib/" -lOpenCL -lm
 
